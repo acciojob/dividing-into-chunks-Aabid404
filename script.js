466e27c36eeffae42e4e5f9d6046f98e2e3a3565
AABID DEVELOPER <150765769+Aabid404@users.noreply.github.com>
2025-07-02 22:57:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,33 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	function divide(arr, n) {
+  const result = [];
+  let current = [];
+  let currentSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    const val = arr[i];
+
+    if (currentSum + val <= n) {
+      current.push(val);
+      currentSum += val;
+    } else {
+      if (current.length > 0) {
+        result.push(current);
+      }
+      current = [val];
+      currentSum = val;
+    }
+  }
+
+  if (current.length > 0) {
+    result.push(current);
+  }
+
+  return result;
+}
+
 };
 
 const n = prompt("Enter n: ");
